76ac5d4ee3d203deeb061b3ebad9af6c3153edf0
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-10-05 18:39:29 -0300

    implemented InMemorySearchableRepository

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -139,5 +139,7 @@ export interface SearchableRepositoryInterface<
   SearchInput = SearchParams,
   SearchOutput = SearchResult<E, Filter>,
 > extends RepositoryInterface<E> {
+  sortableFields: string[]
+
   search(props: SearchInput): Promise<SearchOutput>
 }
