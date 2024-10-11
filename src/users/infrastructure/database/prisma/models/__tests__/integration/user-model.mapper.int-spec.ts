404de6b8de862492743780155aac69cb9bbcfabd
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-10-10 21:05:55 -0300

    UserModelMapper integration tests

---
+++ src/users/infrastructure/database/prisma/models/__tests__/integration/user-model.mapper.int-spec.ts
@@ -0,0 +1,47 @@
+import { PrismaClient, User } from '@prisma/client'
+import { execSync } from 'node:child_process'
+import { UserModelMapper } from '../../user-model.mapper'
+import { ValidationError } from '@/shared/domain/errors/validation-error'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { setupPrismaTests } from '@/shared/infraestructure/database/prisma/testing/setup-prisma-tests'
+
+describe('UserModelMapper integration tests', () => {
+  let prismaService: PrismaClient
+  let props: any
+
+  beforeAll(async () => {
+    setupPrismaTests()
+    prismaService = new PrismaClient()
+    await prismaService.$connect()
+  })
+
+  beforeEach(async () => {
+    await prismaService.user.deleteMany()
+    props = {
+      id: '7ef80c2b-b0e5-44d8-b168-d6a3cf51d93e',
+      name: 'Test name',
+      email: 'a@a.com',
+      password: '1234',
+      createdAt: new Date(),
+    }
+  })
+
+  afterAll(async () => {
+    prismaService.$disconnect()
+  })
+
+  it('should throws error when user model is invalid', async () => {
+    const model: User = Object.assign(props, { name: null })
+    expect(() => UserModelMapper.toEntity(model)).toThrow(
+      new ValidationError('Could not convert to a user entity'),
+    )
+  })
+
+  it('should convert user model to a user entity', async () => {
+    const model: User = await prismaService.user.create({ data: props })
+    const sut = UserModelMapper.toEntity(model)
+
+    expect(sut).toBeInstanceOf(UserEntity)
+    expect(sut.toJSON()).toStrictEqual(props)
+  })
+})
