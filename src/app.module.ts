a7ca4a47d7bf505561a159b29be3f2da512f6375
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-10-16 16:31:13 -0300

    config e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { EnvConfigModule } from './shared/infraestructure/env-config/env-config.module'
 import { UsersModule } from './users/infrastructure/users.module'
 import { DatabaseModule } from './shared/infraestructure/database/database.module'
 
 @Module({
   imports: [EnvConfigModule, UsersModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
