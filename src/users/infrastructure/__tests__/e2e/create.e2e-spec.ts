33d0d94ee69b4f602796fa4cc599cb8a58836d28
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-10-16 20:51:30 -0300

    add validation errors tests on create e2e tests

--- src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
@@ -79,5 +79,58 @@ describe('UsersController e2e tests', () => {
         'password must be a string',
       ])
     })
+
+    it('should return error with 422 code when the name field is invalid', async () => {
+      delete signupDto.name
+      const res = await request(app.getHttpServer())
+        .post('/users')
+        .send(signupDto)
+        .expect(422)
+
+      expect(res.body.error).toBe('Unprocessable Entity')
+      expect(res.body.message).toEqual([
+        'name should not be empty',
+        'name must be a string',
+      ])
+    })
+
+    it('should return error with 422 code when the email field is invalid', async () => {
+      delete signupDto.email
+      const res = await request(app.getHttpServer())
+        .post('/users')
+        .send(signupDto)
+        .expect(422)
+
+      expect(res.body.error).toBe('Unprocessable Entity')
+      expect(res.body.message).toEqual([
+        'email must be an email',
+        'email should not be empty',
+        'email must be a string',
+      ])
+    })
+
+    it('should return error with 422 code when the password field is invalid', async () => {
+      delete signupDto.password
+      const res = await request(app.getHttpServer())
+        .post('/users')
+        .send(signupDto)
+        .expect(422)
+
+      expect(res.body.error).toBe('Unprocessable Entity')
+      expect(res.body.message).toEqual([
+        'password should not be empty',
+        'password must be a string',
+      ])
+    })
+
+    it('should return error with 422 code with invalid field provided', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/users')
+        .send(Object.assign(signupDto, { fake: 'fake' }))
+        .expect(422)
+
+      expect(res.body.error).toBe('Unprocessable Entity')
+      expect(res.body.message).toEqual(['property fake should not exist'])
+    })
   })
 })
