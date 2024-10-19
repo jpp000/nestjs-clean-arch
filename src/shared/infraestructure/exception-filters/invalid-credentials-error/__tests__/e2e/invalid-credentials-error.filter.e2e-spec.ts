4a389cbf87200e5693f12592caf14c1e6b6b8419
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-10-18 22:59:19 -0300

    InvalidCredentialsErrorFilter e2e tests

--- src/shared/infraestructure/exception-filters/invalid-credentials-error/__tests__/e2e/invalid-credentials-error.filter.e2e-spec.ts
+++ src/shared/infraestructure/exception-filters/invalid-credentials-error/__tests__/e2e/invalid-credentials-error.filter.e2e-spec.ts
@@ -1,7 +1,47 @@
+import { Controller, Get, INestApplication } from '@nestjs/common'
 import { InvalidCredentialsErrorFilter } from '../../invalid-credentials-error.filter'
+import { Test, TestingModule } from '@nestjs/testing'
+import { InvalidCredentialsError } from '@/shared/application/errors/invalid-credentials-error copy'
+import request from 'supertest'
 
-describe('InvalidCredentialsErrorFilter', () => {
+@Controller('stub')
+class StubController {
+  @Get()
+  index() {
+    throw new InvalidCredentialsError('Invalid credentials')
+  }
+}
+
+describe('InvalidCredentialsErrorFilter (e2e)', () => {
+  let app: INestApplication
+  let module: TestingModule
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      controllers: [StubController],
+    }).compile()
+
+    app = module.createNestApplication()
+    app.useGlobalFilters(new InvalidCredentialsErrorFilter())
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
   it('should be defined', () => {
     expect(new InvalidCredentialsErrorFilter()).toBeDefined()
+  })
+
+  it('should catch InvalidCredentialsError', async () => {
+    const res = await request(app.getHttpServer())
+      .get('/stub')
+      .expect(400)
+      .expect({
+        statusCode: 400,
+        error: 'Invalid Credentials Error',
+        message: 'Invalid credentials',
+      })
   })
 })
