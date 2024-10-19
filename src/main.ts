dd865ec276acfbc9fac039e524fd58499b64407f
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-10-19 11:39:09 -0300

    swagger config

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,32 @@ import {
 } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
 import { applyGlobalConfig } from './global-config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   )
+
+  const config = new DocumentBuilder()
+    .setTitle('Node.js Course')
+    .setDescription(
+      'Node.js Rest API - NestJS, Typescript, DDD, Clean Architecture and Automated Tests',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Informar o JWT para autorizar o acesso',
+      name: 'Authorization',
+      scheme: 'Bearer',
+      type: 'http',
+      in: 'Header',
+    })
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   applyGlobalConfig(app)
   await app.listen(3000, '0.0.0.0')
 }
